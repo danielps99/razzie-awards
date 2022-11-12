7172591b85a588c57b3ac7865aad9f566d6a8ee8
Daniel <danielps99@gmail.com>
2022-11-12 10:45:42 -0300

    First commit

---
+++ src/main/java/br/com/bdws/razzieawards/RazzieAwardsApplication.java
@@ -0,0 +1,13 @@
+package br.com.bdws.razzieawards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazzieAwardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RazzieAwardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bdws/razzieawards/RazzieAwardsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bdws.razzieawards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazzieAwardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
